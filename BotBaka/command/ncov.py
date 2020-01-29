7df6132b26b4a0b6d85928e92968170faa194ae2
lightless <lightless@foxmail.com>
2020-01-29 10:11:30 +0800

    [ADD] bug fix

--- BotBaka/command/ncov.py
+++ BotBaka/command/ncov.py
@@ -37,6 +37,46 @@ class NCovCommand(BaseCommand):
 
         self.error_msg = "格式错误\n%ncov 浙江 - 显示浙江数据\n%ncov list - 显示所有省份详细信息\n%ncov all - 全国统计信息"
 
+        self.url = "https://3g.dxy.cn/newh5/view/pneumonia"
+
+    def __get_bs(self):
+        response = requests.get(self.url, timeout=12)
+        soup = BeautifulSoup(response.content)
+        return soup
+
+    def __get_area_stat(self, soup):
+        text = soup.find(id="getAreaStat").text
+        pattern = r"window\.getAreaStat = (.+)}catch\(e\){}"
+        json_data = re.search(pattern, text).group(1)
+        json_data = json.loads(json_data)
+        return json_data
+
+    def __get_global_stat(self, soup):
+        data = soup.find(id='getStatisticsService').text
+        pattern = r"window\.getStatisticsService\s*=\s*(.+)}catch\(e\){}"
+        j = re.search(pattern, data).group(1)
+        j_data = json.loads(j)
+        return j_data
+
+    def __format_item(self, item):
+        province_name = item.get("provinceName")
+
+        message = "【{}】\n".format(province_name)
+        message += "确认病例：{}\n".format(item.get("confirmedCount"))
+        message += "治愈病例：{}\n".format(item.get("curedCount"))
+        message += "死亡病例：{}\n".format(item.get("deadCount"))
+        message += "==========\n"
+        message += "【{}】中各城市统计数据：\n".format(province_name)
+        for city_data in item.get("cities"):
+            message += "【{}】确认：{}，治愈：{}，死亡：{}\n".format(
+                city_data.get("cityName"),
+                city_data.get("confirmedCount"),
+                city_data.get("curedCount"),
+                city_data.get("deadCount"),
+            )
+
+        return message
+
     def process(self, from_group: int, from_qq: int, name: str, command_list: List[str]):
 
         try:
@@ -45,72 +85,33 @@ class NCovCommand(BaseCommand):
             self.CQApi.send_group_message(from_group, from_qq, self.error_msg)
             return
 
-        url = "https://3g.dxy.cn/newh5/view/pneumonia"
-        response = requests.get(url, timeout=12)
-        soup = BeautifulSoup(response.content)
-        text = soup.find(id="getAreaStat").text
-        pattern = r"window\.getAreaStat = (.+)}catch\(e\){}"
-        j = re.search(pattern, text).group(1)
-        results = json.loads(j)
-
-        # 全国信息
-        global_data = soup.find(class_="content___2hIPS").find_all('span')
+        soup = self.__get_bs()
 
         if province == "list":
-
+            results = self.__get_area_stat(soup)
             message = ""
-
             for p_data in results:
-                province_name = p_data.get("provinceName")
-                message += "【{}】\n".format(province_name)
-                message += "确认病例：{}\n".format(p_data.get("confirmedCount"))
-                message += "治愈病例：{}\n".format(p_data.get("curedCount"))
-                message += "死亡病例：{}\n".format(p_data.get("deadCount"))
-                message += "==========\n"
-                message += "【{}】中各城市统计数据：\n".format(province_name)
-                for city_data in p_data.get("cities"):
-                    message += "【{}】确认：{}，治愈：{}，死亡：{}\n".format(
-                        city_data.get("cityName"),
-                        city_data.get("confirmedCount"),
-                        city_data.get("curedCount"),
-                        city_data.get("deadCount"),
-                    )
+                message += self.__format_item(p_data) + "\n"
 
             self.CQApi.send_group_message(from_group, from_qq, message)
-
         elif province == "all":
-            stat = []
-            for idx, tag_span in enumerate(global_data):
-                if idx % 2 == 0:
-                    continue
-
-                num = tag_span.text
-                stat.append(num)
+            j_data = self.__get_global_stat(soup)
 
             message = "【全国统计】\n"
-            message += "确诊：{}\n".format(stat[0])
-            message += "疑似：{}\n".format(stat[1])
-            message += "死亡：{}\n".format(stat[2])
-            message += "治愈：{}\n".format(stat[3])
-            self.CQApi.send_group_message(from_group, from_qq, message)
+            message += "确诊：{}\n".format(j_data.get("confirmedCount"))
+            message += "疑似：{}\n".format(j_data.get("suspectedCount"))
+            message += "死亡：{}\n".format(j_data.get("deadCount"))
+            message += "治愈：{}\n".format(j_data.get("curedCount"))
 
+            self.CQApi.send_group_message(from_group, from_qq, message)
         else:
+
+            results = self.__get_area_stat(soup)
+
             for item in results:
                 province_name = item.get("provinceName")
                 province_short_name = item.get("provinceShortName")
                 if province in (province_name, province_short_name):
-                    message = "【{}】\n".format(province_name)
-                    message += "确认病例：{}\n".format(item.get("confirmedCount"))
-                    message += "治愈病例：{}\n".format(item.get("curedCount"))
-                    message += "死亡病例：{}\n".format(item.get("deadCount"))
-                    message += "==========\n"
-                    message += "【{}】中各城市统计数据：\n".format(province_name)
-                    for city_data in item.get("cities"):
-                        message += "【{}】确认：{}，治愈：{}，死亡：{}\n".format(
-                            city_data.get("cityName"),
-                            city_data.get("confirmedCount"),
-                            city_data.get("curedCount"),
-                            city_data.get("deadCount"),
-                        )
-
+                    message = self.__format_item(item)
                     self.CQApi.send_group_message(from_group, from_qq, message)
+                    return
